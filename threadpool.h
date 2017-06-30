ac336b2fa711168168b8670247ee8cfeb32cef90
Wolfram Rösler <wolfram@roesler-ac.de>
2017-06-30 09:59:56 +0200

    Add comments, plus minor formal changes

--- threadpool.h
+++ threadpool.h
@@ -1,5 +1,16 @@
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
+/**
+ *  \brief     C++ thread pool
+ *  \details   Supply a number of threads and distribute work to them.
+ *  \author    Jakob Progsch, Václav Zeman
+ *  \author    modified by Wolfram Rösler
+ *  \date      2017-06-30
+ *  \copyright Free for all - details on https://raw.githubusercontent.com/wolframroesler/ThreadPool/master/COPYING
+ *
+ * This version: https://github.com/wolframroesler/ThreadPool
+ * Original version: https://github.com/progschj/ThreadPool
+ */
+
+#pragma once
 
 #include <vector>
 #include <queue>
@@ -11,6 +22,20 @@
 #include <functional>
 #include <stdexcept>
 
+/**
+ * Thread pool class.
+ *
+ * Example (in lieu of proper documentation):
+ *
+ *      // create thread pool with 4 worker threads
+ *      ThreadPool pool(4);
+ *
+ *      // enqueue and store future
+ *      auto result = pool([](int answer) { return answer; }, 42);
+ *
+ *      // get result from future
+ *      std::cout << result.get() << std::endl;
+ */
 class ThreadPool {
 public:
     ThreadPool(size_t);
@@ -22,25 +47,22 @@ public:
 private:
     // need to keep track of threads so we can join them
     std::vector< std::thread > workers;
+    
     // the task queue
     std::queue< std::function<void()> > tasks;
     
     // synchronization
     std::mutex queue_mutex;
     std::condition_variable condition;
-    bool stop;
+    bool stop = false;
 };
  
 // the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
-    :   stop(false)
-{
-    for(size_t i = 0;i<threads;++i)
+inline ThreadPool::ThreadPool(size_t threads) {
+    for(size_t i=0;i<threads;++i) {
         workers.emplace_back(
-            [this]
-            {
-                for(;;)
-                {
+            [this] {
+                for(;;) {
                     std::function<void()> task;
 
                     {
@@ -57,6 +79,7 @@ inline ThreadPool::ThreadPool(size_t threads)
                 }
             }
         );
+    }
 }
 
 // add new work item to the pool
@@ -85,8 +108,7 @@ auto ThreadPool::operator()(F&& f, Args&&... args)
 }
 
 // the destructor joins all threads
-inline ThreadPool::~ThreadPool()
-{
+inline ThreadPool::~ThreadPool() {
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
         stop = true;
@@ -95,5 +117,3 @@ inline ThreadPool::~ThreadPool()
     for(std::thread &worker: workers)
         worker.join();
 }
-
-#endif
