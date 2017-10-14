088d0de063723589ebfca2d3fa0fc215dbe3a104
Wolfram Rösler <wolfram@roesler-ac.de>
2017-10-14 12:54:44 +0200

    Add command line parameter parsing

--- main.cpp
+++ main.cpp
@@ -6,9 +6,26 @@
 
 #include <iostream>
 
-/*
+/**
+ * Demonstration 1: Simple std::thread example
+ */
+void Demo1() {
+	std::cout << "TBD\n";
+}
+
+/**
  * Program starts here
  */
-int main() {
-	std::cout << "Hello World\n";
+int main(int argc,char** argv) {
+	switch(argc==2 ? atoi(argv[1]) : -1) {
+		case 1: Demo1(); break;
+
+		default:
+		  std::cout
+			<< "C++11 multithreading demonstration program\n"
+			<< "Usage:\n"
+			<< "\t" << argv[0] << " 1\tSimple std::thread example\n"
+			;
+		  return EXIT_FAILURE;
+	}
 }
