76bd24f014abc4a9d49fc20378513f89c40d7b70
Wolfram Rösler <wolfram@roesler-ac.de>
2017-10-14 12:46:34 +0200

    Add compilable demo program and cmake file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @brief C++11 multithreading demonstration program
+ * @author Wolfram Rösler <wolfram@roesler-ac.de>
+ * @date 2017-10-14
+ */
+
+#include <iostream>
+
+/*
+ * Program starts here
+ */
+int main() {
+	std::cout << "Hello World\n";
+}
